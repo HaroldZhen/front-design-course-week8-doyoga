55326b50be32cfd7440645095898082fad3e0003
harold jhan <haroldzhen531@gmail.com>
2021-08-24 14:03:29 +0800

    surroundings 環境空間

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,1 +1,14 @@
 console.log('Hello!');
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // autoplay: {
+  //   delay: 2000,
+  // },
+  // direction: 'vertical',
+});
