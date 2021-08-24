b999a76193c09f98afdac8c7dc50b1e5118dab5a
harold jhan <haroldzhen531@gmail.com>
2021-08-24 19:48:23 +0800

    course

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,6 +1,6 @@
 console.log('Hello!');
 const swiper = new Swiper('.swiper-container', {
-  loop: true,
+  // loop: true,
   slidesPerView: 'auto',
   spaceBetween: 30,
   pagination: {
@@ -12,3 +12,14 @@ const swiper = new Swiper('.swiper-container', {
   // },
   // direction: 'vertical',
 });
+isMobile();
+window.addEventListener('resize', isMobile);
+function isMobile(){
+  if (document.documentElement.clientWidth > 767) {
+    swiper.changeDirection('horizontal');
+  } else {
+    swiper.changeDirection('vertical');
+  }
+  // console.log(`clientWidth: ${document.documentElement.clientWidth}`);
+  // console.log(`clientHeight: ${document.documentElement.clientHeight}`);
+}
