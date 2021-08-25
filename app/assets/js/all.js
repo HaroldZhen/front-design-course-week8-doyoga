124597c5f633e1d91d0141702f89d5b1e91cf2ff
harold jhan <haroldzhen531@gmail.com>
2021-08-25 15:12:08 +0800

    index swiper 2

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -12,7 +12,6 @@ const swiper = new Swiper('.swiper-container', {
   // },
   // direction: 'vertical',
 });
-isMobile();
 window.addEventListener('resize', isMobile);
 function isMobile(){
   if (document.documentElement.clientWidth > 767) {
@@ -32,16 +31,17 @@ const swiper2 = new Swiper('.swiper-container-project', {
     el: ".swiper-pagination",
     clickable: true,
   },
+  direction: 'horizontal',
 });
-isMobile2();
-window.addEventListener('resize', isMobile2);
-function isMobile2(){
-  if (document.documentElement.clientWidth > 767) {
-    swiper2.changeDirection('horizontal');
-  } else {
-    swiper2.changeDirection('vertical');
-  }
-}
+// isMobile2();
+// window.addEventListener('resize', isMobile2);
+// function isMobile2(){
+//   if (document.documentElement.clientWidth > 768) {
+//     swiper2.changeDirection('horizontal');
+//   } else {
+//     swiper2.changeDirection('vertical');
+//   }
+// }
 
 const swiper3 = new Swiper('.swiper-container-course', {
   // loop: true,
@@ -54,4 +54,23 @@ const swiper3 = new Swiper('.swiper-container-course', {
   autoplay: {
     delay: 2000,
   },
+});
+
+const swiper4 = new Swiper('.swiper-container-testimonials', {
+  slidesPerView: '1',
+  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 1280px
+    1280: {
+      slidesPerView: 2,
+    }
+  },
+  // autoplay: {
+  //   delay: 2000,
+  // },
+  direction: 'horizontal',
 });
