1c6acbac679fc5194ac25a0b0aa7b31235ca2c46
harold jhan <haroldzhen531@gmail.com>
2021-08-27 21:05:30 +0800

    Update 2021-08-27T13:05:23.817Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -99,6 +99,39 @@ var swiper4 = new Swiper('.swiper-container-testimonials', {
 });
 "use strict";
 
+(function (d) {
+  var config = {
+    kitId: 'zma3hfb',
+    scriptTimeout: 3000,
+    async: true
+  },
+      h = d.documentElement,
+      t = setTimeout(function () {
+    h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+  }, config.scriptTimeout),
+      tk = d.createElement("script"),
+      f = false,
+      s = d.getElementsByTagName("script")[0],
+      a;
+  h.className += " wf-loading";
+  tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+  tk.async = true;
+
+  tk.onload = tk.onreadystatechange = function () {
+    a = this.readyState;
+    if (f || a && a != "complete" && a != "loaded") return;
+    f = true;
+    clearTimeout(t);
+
+    try {
+      Typekit.load(config);
+    } catch (e) {}
+  };
+
+  s.parentNode.insertBefore(tk, s);
+})(document);
+"use strict";
+
 function socialAlert(e) {
   e.preventDefault();
   Swal.fire({
@@ -181,37 +214,4 @@ function searchResult() {
     timer: 1500
   });
 }
-"use strict";
-
-(function (d) {
-  var config = {
-    kitId: 'zma3hfb',
-    scriptTimeout: 3000,
-    async: true
-  },
-      h = d.documentElement,
-      t = setTimeout(function () {
-    h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
-  }, config.scriptTimeout),
-      tk = d.createElement("script"),
-      f = false,
-      s = d.getElementsByTagName("script")[0],
-      a;
-  h.className += " wf-loading";
-  tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
-  tk.async = true;
-
-  tk.onload = tk.onreadystatechange = function () {
-    a = this.readyState;
-    if (f || a && a != "complete" && a != "loaded") return;
-    f = true;
-    clearTimeout(t);
-
-    try {
-      Typekit.load(config);
-    } catch (e) {}
-  };
-
-  s.parentNode.insertBefore(tk, s);
-})(document);
 //# sourceMappingURL=all.js.map
